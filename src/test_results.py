86e0b1dc15ccc531d48964ab47abdc790f3b970d
Oleksandr <nedosiekoleksandr@gmail.com>
2026-05-04 23:48:17 +0200

    test-results.py is changed

--- src/test_results.py
+++ src/test_results.py
@@ -20,21 +20,30 @@ class SimpleModel(nn.Module):
         self.conv1 = nn.Conv2d(3, 32, 3, padding=1)
         self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
         self.conv3 = nn.Conv2d(64, 128, 3, padding=1)
+        self.conv4 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+
+        self.bn1 = nn.BatchNorm2d(32)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.bn4 = nn.BatchNorm2d(256)
+
         self.pool = nn.MaxPool2d(2, 2)
         self.dropout = nn.Dropout(0.5)
-        self.fc1 = nn.Linear(128 * 28 * 28, 512)
+
+        self.fc1 = nn.Linear(256 * 16 * 16, 512)
         self.fc2 = nn.Linear(512, liczba_budynkow)
 
     def forward(self, x):
-        x = self.pool(torch.relu(self.conv1(x)))  # 224 -> 112
-        x = self.pool(torch.relu(self.conv2(x)))  # 112 -> 56
-        x = self.pool(torch.relu(self.conv3(x)))  # 56 -> 28
-        x = x.view(x.size(0), -1)
-        x = self.dropout(x)
+        x = self.pool(torch.relu(self.bn1(self.conv1(x))))
+        x = self.pool(torch.relu(self.bn2(self.conv2(x))))
+        x = self.pool(torch.relu(self.bn3(self.conv3(x))))
+        x = self.pool(torch.relu(self.bn4(self.conv4(x))))
+
+        x = x.view(x.size(0), -1)  # Spłaszczanie
         x = torch.relu(self.fc1(x))
+        x = self.dropout(x)
         x = self.fc2(x)
         return x
-
 
 # 3. Parametry i ścieżki
 Model_src = 'src/model_pwr_13_04_2026.pth'
